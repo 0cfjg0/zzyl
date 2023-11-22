5c25dfcb8a9b593aa997d83a334eae3bd246c2ce
张志君 <mail.zhijun.zhang@icloud.com>
2023-11-22 10:12:19 +0800

    整理学生学习代码

--- zzyl-service/src/main/java/com/zzyl/dto/NursingProjectDto.java
+++ zzyl-service/src/main/java/com/zzyl/dto/NursingProjectDto.java
@@ -1,7 +1,6 @@
 package com.zzyl.dto;
 
 import com.zzyl.base.BaseDto;
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -15,42 +14,35 @@ public class NursingProjectDto extends BaseDto {
     /**
      * 名称
      */
-    @ApiModelProperty(value = "名称")
     private String name;
 
     /**
      * 排序号
      */
-    @ApiModelProperty(value = "排序号")
     private Integer orderNo;
 
     /**
      * 单位
      */
-    @ApiModelProperty(value = "单位")
     private String unit;
 
     /**
      * 价格
      */
-    @ApiModelProperty(value = "价格")
     private BigDecimal price;
 
     /**
      * 图片
      */
-    @ApiModelProperty(value = "图片")
     private String image;
 
     /**
      * 护理要求
      */
-    @ApiModelProperty(value = "护理要求")
     private String nursingRequirement;
 
     /**
      * 状态（0：禁用，1：启用）
      */
-    @ApiModelProperty(value = "状态（0：禁用，1：启用）")
     private Integer status;
 }
